40b8e7d34d9e9c0f5095ed4e816c5d191642f57c
unknown <krishnachavda903@gmail.com>
2026-01-30 13:49:03 +0530

    [krc] [project update succesfully]

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './componets/Header'
+import Register from './pages/register'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,8 @@ function App() {
     <>
       
     <Header />
+    <Register/>
+    
     </>
   )
 }
