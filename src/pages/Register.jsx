589ef293adc5d6296d8116707bf902e5f2dd7741
unknown <krishnachavda903@gmail.com>
2026-02-02 12:08:07 +0530

    [krc] [project update succesfully]

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,61 +1,141 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Register.css";
-import {Link} from "react-router-dom";
 
 const Register = () => {
+  //declaration
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phone: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  const navigate= useNavigate()
+
+  //logic
+  const handleInputChange = (e) => {
+    //console.log(e.target.name,e.target.value)
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+
+    setErrors({
+      ...errors,
+      [e.target.name]:""
+    })
+  };
+
+  const validate = () => {
+    const newErrors = {};
+    if (!formData.name.trim()) {
+      newErrors.name = "Full name is required.";
+    } else if (formData.name.length <= 3) {
+      newErrors.name = "Minimum 3 character required.";
+    }
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required.";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      newErrors.email = "Invalid Email format.";
+    }
+    if (!formData.phone.trim()) {
+      newErrors.phone = "Phone number is required.";
+    } else if (!/^[0-9]{10}$/.test(formData.phone)) {
+      newErrors.phone = "Phone must be in 10 digit.";
+    }
+    if (!formData.password.trim()) {
+      newErrors.password = "Password is required.";
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Minimum 6 character required.";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      localStorage.setItem('authData',JSON.stringify(formData))
+      alert('Registration success')
+      navigate("/login")
+    }
+    
+  };
+
+  //useEffect(() => {
+  //  console.log(formData);
+  //}, [formData]);
+
+  //design
   return (
     <>
       <div className="form-container">
         <h1 className="form-title">REGISTER</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
+          {/* name field */}
           <div className="form-group">
-            <lable htmlFor="name">Full Name</lable>
+            <label htmlFor="name">Full Name</label>
             <input
               type="text"
-              id="name"
               name="name"
+              id="name"
+              value={formData.name}
               placeholder="Enter your full name"
+              onChange={handleInputChange}
             />
+            {errors.name && <span className="error-msg">{errors.name}</span>}
           </div>
-          {/*email field*/}
+          {/*email field */}
           <div className="form-group">
-            <lable htmlFor="email">Email Address</lable>
-
+            <label htmlFor="email">Email Address</label>
             <input
               type="email"
-              id="email"
               name="email"
+              id="email"
+              value={formData.email}
               placeholder="Enter your email"
+              onChange={handleInputChange}
             />
+            {errors.email && <span className="error-msg">{errors.email}</span>}
           </div>
-
-          {/*phone Number Field*/}
+          {/*phone number field */}
           <div className="form-group">
-            <lable htmlFor="phone">phone Number</lable>
+            <label htmlFor="phone">Phone Number</label>
             <input
               type="tel"
-              id="phone"
               name="phone"
-              placeholder="create a password"
+              id="phone"
+              value={formData.phone}
+              placeholder="Enter your phone number"
+              onChange={handleInputChange}
             />
+            {errors.phone && <span className="error-msg">{errors.phone}</span>}
           </div>
-          {/* password Filed*/}
+          {/*password field */}
           <div className="form-group">
-            <lable htmlFor="password">password</lable>
+            <label htmlFor="password">Password</label>
             <input
               type="password"
               name="password"
               id="password"
-              placeholder="create a password"
+              value={formData.password}
+              placeholder="Creat a password"
+              onChange={handleInputChange}
             />
+            {errors.password && (
+              <span className="error-msg">{errors.password}</span>
+            )}
           </div>
-          {/*submit button*/}
+          {/*submit button */}
           <button type="submit" className="btn-primary">
             Register
           </button>
         </form>
+        {/*link to login page */}
         <p className="link-text">
-          Already have an Account? <Link to="/login">Login Here</Link>
+          Already have an account? <Link to="/login">Login Here</Link>
         </p>
       </div>
     </>
