428805c8b4666e3619c4368b2241b6a79a0cd5da
unknown <krishnachavda903@gmail.com>
2026-02-04 13:40:23 +0530

    [krc] [project update succesfully]

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "../componets/Navbar";
+import { Navigate, useNavigate } from "react-router-dom";
+import TaskList from "../componets/TaskList";
 
 const Dashboard = () => {
-  return (
-  <div>Heloo World</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Dashboard
+  const [data, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const responce = await fetch("http://localhost:3000/task");
+      const data = Response.json();
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("authData");
+    localStorage.removeItem("Data");
+    //localStorage.clear()
+    navigate("/login");
+  };
+  return (
+    <div>
+      <Navbar title="Task Manager" onLogout={handleLogout} />
+      <h1>My Task</h1>
+      <TaskList />
+    </div>
+  );
+};
+
+export default Dashboard;
